4fdb58be932200dc491b61b2a4967fe674d1b4df
dat.truong <truongquocdat196nt@gmail.com>
2021-12-11 16:44:01 +0700

    chore nav-update

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,16 +1,26 @@
 import React from "react";
 import 'tachyons'
 
-const Navigation = ({onRouteChange, route}) => {
+const NavDisplay = (route, onRouteChange) => {
+	console.log(route);
+	switch(route) {
+		case 'signin':
+			return <p onClick={() => onRouteChange('register')} className="f3 underline dim black pa3 pointer">Register</p>;
+		case 'home':
+			return <p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign out</p>;
+		case 'signout':
+			return <p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign out</p>;
+		case 'register':
+			return <p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign in</p>;
+		default:
+			return;
+	}
+}
+
+const Navigation = ({route, onRouteChange}) => {
 	return (
 		<nav style={{display: 'flex', justifyContent:'flex-end'}}>
-			{route === 'signin'
-			? <>
-				<p onClick={() => onRouteChange('register')} className="f3 underline dim black pa3 pointer">Register</p>
-				<p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign out</p>
-			</>
-			: <p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign in</p>
-			}
+		{NavDisplay(route, onRouteChange)}
 		</nav>
 	);
 }
