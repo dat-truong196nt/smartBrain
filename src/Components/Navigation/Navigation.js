13d898d98d649695be3cf38f23cdc04c71fc1098
dat.truong <truongquocdat196nt@gmail.com>
2021-12-17 04:41:23 +0700

    chore update-nav

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,15 +1,15 @@
 import React from "react";
 import 'tachyons'
 
-const NavDisplay = (route, onRouteChange) => {
+const NavDisplay = (route, onRouteChange, onSignOut) => {
 	console.log(route);
 	switch(route) {
 		case 'signin':
 			return <p onClick={() => onRouteChange('register')} className="f3 underline dim black pa3 pointer">Register</p>;
 		case 'home':
-			return <p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign out</p>;
+			return <p onClick={() => {onSignOut();onRouteChange('signin')}} className="f3 underline dim black pa3 pointer">Sign out</p>;
 		case 'signout':
-			return <p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign out</p>;
+			return <p onClick={() => {onSignOut();onRouteChange('signin')}} className="f3 underline dim black pa3 pointer">Sign out</p>;
 		case 'register':
 			return <p onClick={() => onRouteChange('signin')} className="f3 underline dim black pa3 pointer">Sign in</p>;
 		default:
@@ -17,10 +17,10 @@ const NavDisplay = (route, onRouteChange) => {
 	}
 }
 
-const Navigation = ({route, onRouteChange}) => {
+const Navigation = ({route, onRouteChange, onSignOut}) => {
 	return (
 		<nav style={{display: 'flex', justifyContent:'flex-end'}}>
-		{NavDisplay(route, onRouteChange)}
+		{NavDisplay(route, onRouteChange, onSignOut)}
 		</nav>
 	);
 }
