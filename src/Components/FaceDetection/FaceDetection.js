c6348dbc40ad2dcae80b2694936de2a625455016
dat.truong <truongquocdat196nt@gmail.com>
2021-12-17 04:40:41 +0700

    chore remove-warning

--- src/Components/FaceDetection/FaceDetection.js
+++ src/Components/FaceDetection/FaceDetection.js
@@ -2,7 +2,7 @@ import React from "react";
 import './FaceDetection.scss'
 import "tachyons";
 
-const createBoundingBoxs = (boxs) => boxs.reduce(
+const createBoundingBoxs = (childboxs) => childboxs.reduce(
 	(acc, box) => {
 		acc.push(<div className='bounding-box' style={{top: box.top, bottom: box.bottom, left: box.left, right: box.right}}></div>);
 		return acc;
@@ -12,7 +12,7 @@ const createBoundingBoxs = (boxs) => boxs.reduce(
 const FaceDetection = ({imageUrl, boxs}) => {
 	return imageUrl ? (
 		<div className="pa3 ma3" style={{ display: "flex", justifyContent: "center" }}>
-			<div className='absolute'>
+			<div key='anonymous' className='absolute'>
 				<img id='imgToDetect' alt="img-detect" src={imageUrl} style={{width: '500px', height: 'auto'}}></img>
 				{createBoundingBoxs(boxs)}
 			</div>
